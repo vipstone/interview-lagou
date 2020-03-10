0b0941e880f4396f0559ab071a782a6f1b48a447
stone <295628068@qq.com>
2020-03-10 13:55:39 +0800

    添加线程池扩展

--- src/main/java/com/lagou/interview/ThreadPoolExample.java
+++ src/main/java/com/lagou/interview/ThreadPoolExample.java
@@ -1,20 +1,73 @@
 package com.lagou.interview;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class ThreadPoolExample {
     public static void main(String[] args) {
-        // 模拟线程池溢出
-        Executor executor = new ThreadPoolExecutor(2, 4, 10L, TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(1), new ThreadPoolExecutor.AbortPolicy());
+        // 线程池扩展
+        MyThreadPoolExecutor executor = new MyThreadPoolExecutor(2, 4, 10, TimeUnit.SECONDS,
+                new LinkedBlockingQueue());
+        for (int i = 0; i < 3; i++) {
+            executor.execute(() -> {
+                Thread.currentThread().getName();
+            });
+        }
+
+        // 线程池溢出演示
+        overflow();
+    }
+
+    /**
+     * 线程池溢出演示
+     */
+    private static void overflow() {
+        Executor executor = new ThreadPoolExecutor(2, 4, 10L,
+                TimeUnit.SECONDS, new LinkedBlockingQueue<>(1), new ThreadPoolExecutor.AbortPolicy());
         for (int i = 0; i < 6; i++) {
             executor.execute(() -> {
                 System.out.println("当前线程：" + Thread.currentThread().getName());
             });
         }
-
     }
+
+    /**
+     * 线程池扩展
+     */
+    static class MyThreadPoolExecutor extends ThreadPoolExecutor {
+        private final ThreadLocal<Long> localTime = new ThreadLocal<>();
+
+        public MyThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+        }
+
+        /**
+         * 开始执行之前
+         * @param t 线程
+         * @param r 任务
+         */
+        @Override
+        protected void beforeExecute(Thread t, Runnable r) {
+            Long sTime = System.nanoTime(); // 开始时间 (单位：纳秒)
+            localTime.set(sTime);
+            System.out.println(String.format("%s | before | time=%s",
+                    t.getName(), sTime));
+            super.beforeExecute(t, r);
+        }
+
+        /**
+         * 执行完成之后
+         * @param r 任务
+         * @param t 抛出的异常
+         */
+        @Override
+        protected void afterExecute(Runnable r, Throwable t) {
+            System.out.println(t);
+            Long eTime = System.nanoTime(); // 结束时间 (单位：纳秒)
+            Long totalTime = eTime - localTime.get(); // 执行总时间
+            System.out.println(String.format("%s | after | time=%s | 耗时：%s 毫秒",
+                    Thread.currentThread().getName(), eTime, (totalTime / 1000000.0)));
+            super.afterExecute(r, t);
+        }
+    }
+
 }
