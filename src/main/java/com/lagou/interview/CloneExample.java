993298c988be108d3fba59eb164bb15ef3aa4f2c
wanglei <295628068@qq.com>
2020-03-19 17:48:45 +0800

    添加 clone 示例

---
+++ src/main/java/com/lagou/interview/CloneExample.java
@@ -0,0 +1,55 @@
+package com.lagou.interview;
+
+/**
+ * 克隆相关示例
+ */
+public class CloneExample {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        // 创建被赋值对象
+        People p1 = new People();
+        p1.setId(1);
+        p1.setName("Java");
+        // 克隆 p1 对象
+        People p2 = (People) p1.clone();
+        // 打印名称
+        System.out.println("p2:" + p2.getName());
+
+
+        p2.setName("Lang");
+        System.out.println(p1.getName());
+        System.out.println("p2:" + p2.getName());
+
+//        Arrays.copyOf()
+    }
+
+    static class People implements Cloneable {
+        private Integer id;
+        private String name;
+
+        /**
+         * 重写 clone 方法
+         * @throws CloneNotSupportedException
+         */
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone();
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
