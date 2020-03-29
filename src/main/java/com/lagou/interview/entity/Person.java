b8e248b9d02529b251c85b112e06a4d135c9b728
wanglei <295628068@qq.com>
2020-03-29 14:41:07 +0800

    Lombok 示例完善

--- src/main/java/com/lagou/interview/entity/Person.java
+++ src/main/java/com/lagou/interview/entity/Person.java
@@ -1,10 +1,8 @@
 package com.lagou.interview.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class Person {
     private Integer id;
     private String name;
